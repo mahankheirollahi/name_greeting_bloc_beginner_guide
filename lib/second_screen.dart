688c0f32ae7747ca902d9ab0f11be7c0c6c2057e
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-03 08:43:44 +0300

    Add ui

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:name_greeting_bloc_beginner_guide/bloc/greeting_bloc.dart';
 
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: context.read<GreetingBloc>().state.favoriteColor,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Name Greeting',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          "Welcome ${context.read<GreetingBloc>().state.name} 👋",
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 28,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
