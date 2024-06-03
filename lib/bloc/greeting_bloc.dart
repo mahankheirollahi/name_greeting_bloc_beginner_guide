13565ddfbf9d0315bdc617b11f35be2ab411071e
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-03 08:33:48 +0300

    Add methods emit state

--- lib/bloc/greeting_bloc.dart
+++ lib/bloc/greeting_bloc.dart
@@ -1,13 +1,20 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'dart:async';
+import 'dart:ui';
 
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 part 'greeting_event.dart';
 part 'greeting_state.dart';
 
 class GreetingBloc extends Bloc<GreetingEvent, GreetingState> {
-  GreetingBloc() : super(GreetingInitial()) {
-    on<GreetingEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  GreetingBloc() : super(GreetingState.initial()) {
+    on<GetName>(_getName);
+    on<GetFavoriteColor>(_getFavoriteColor);
   }
+
+  FutureOr<void> _getName(GetName event, Emitter<GreetingState> emit) {}
+
+  FutureOr<void> _getFavoriteColor(
+      GetFavoriteColor event, Emitter<GreetingState> emit) {}
 }
