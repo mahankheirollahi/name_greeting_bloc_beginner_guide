a65cdb025488647c6c089a66935586c42c55d604
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-03 08:35:24 +0300

    Inject GreetingBloc into widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:name_greeting_bloc_beginner_guide/bloc/greeting_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(BlocProvider(
+    create: (context) => GreetingBloc(),
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +15,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
