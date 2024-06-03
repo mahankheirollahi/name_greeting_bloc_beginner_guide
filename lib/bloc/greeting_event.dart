6b979dadfc2809c826ccdf60360610fd9bc7d887
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-03 08:32:35 +0300

    Add get name and get favorite color events

--- lib/bloc/greeting_event.dart
+++ lib/bloc/greeting_event.dart
@@ -6,3 +6,13 @@ sealed class GreetingEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class GetName extends GreetingEvent {
+  final String name;
+  const GetName({required this.name});
+}
+
+class GetFavoriteColor extends GreetingEvent {
+  final Color favoriteColor;
+  const GetFavoriteColor({required this.favoriteColor});
+}
